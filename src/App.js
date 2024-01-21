6507e3ef787fef6ca7529099c593cabfd660cd77
mj-gsl <gsl.produktiion@gmail.com>
2024-01-21 18:10:36 +0100

    Finall

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css'; // Custom CSS file for additional styling
+import './App.css'; 
 
 function App() {
   const [tasks, setTasks] = useState([]);
